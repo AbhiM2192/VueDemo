6ff5f9728ead972f0748be237850ae8f7880d07a
AbhiM2192 <aece2192@gmail.com>
2021-05-10 11:05:35 +0530

    Adding code for slots and prettier config

--- demo-vue/src/components/ComponentsEventsDemo/Popup.vue
+++ demo-vue/src/components/ComponentsEventsDemo/Popup.vue
@@ -1,14 +1,28 @@
 <template>
     <div>
         <h2>This is a Popup</h2>
-        <button @click="$emit('closePopup','Data from PopupComponent')">Close Popup</button>
+        <input type="text" v-model="name">
+        <button @click="$emit('closePopup',name)">Close Popup</button>
     </div>
 </template>
 
 <script>
     export default {
         name:'Popup',
-        emits:['closePopup']
+        emits:{
+            closePopup: (name) =>{
+                if(!name){
+                    return false
+                } else {
+                    return true
+                }
+            }
+        },
+        data(){
+            return{
+                name:''
+            }
+        }
     }
 </script>
 
---
+++ demo-vue/src/components/SlotPropsDemo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h3 v-for="name in names " :key="name.fName">
+            {{name.fName}} || {{name.lName}}
+        </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'SlotPropsDemo',
+        data(){
+            return {
+                names:[
+                    {fName:'Bruce',lName:'Wayne'},
+                    {fName:'Tony',lName:'Stark'},
+                    {fName:'Dr.',lName:'Strange'}
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
